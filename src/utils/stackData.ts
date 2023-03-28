150ba419d05f4b1d20ed4243c6da8279d7c8a556
Guilherme Felipe <guilhermefelipecas@gmail.com>
2023-03-28 14:06:36 -0300

    feat: adicionando dados e customizando cores

--- src/utils/stackData.ts
+++ src/utils/stackData.ts
@@ -4,36 +4,37 @@ import imgJS from "../public/static/img/stack/js.svg";
 import imgNode from "../public/static/img/stack/node.svg";
 import imgStyled from "../public/static/img/stack/styled.svg";
 import imgReact from "../public/static/img/stack/react.svg";
-import { FaVuejs } from "react-icons/fa";
-import { SiTypescript } from "react-icons/si";
-import { FaDocker } from "react-icons/fa";
+import { SiChakraui, SiTypescript } from "react-icons/si";
+import { DiPython, DiDjango, DiPostgresql } from "react-icons/di";
 
 export const stackData = [
-  {
-    title: "HTML",
-    img: imgHTML,
-  },
-  {
-    title: "CSS",
-    img: imgCSS,
-  },
-  {
-    title: "JS",
-    img: imgJS,
-  },
-  {
-    title: "Node JS",
-    img: imgNode,
-  },
-  {
-    title: "Styled Components",
-    img: imgStyled,
-  },
-  {
-    title: "React",
-    img: imgReact,
-  },
-  { title: "Vuejs", img: FaVuejs },
-  { title: "TypeScript", img: SiTypescript },
-  { title: "Docker", img: FaDocker },
+	{
+		title: "HTML",
+		img: imgHTML,
+	},
+	{
+		title: "CSS",
+		img: imgCSS,
+	},
+	{
+		title: "JS",
+		img: imgJS,
+	},
+	{ title: "TypeScript", img: SiTypescript },
+	{
+		title: "Styled Components",
+		img: imgStyled,
+	},
+	{
+		title: "React",
+		img: imgReact,
+	},
+	{ title: "Chakra-ui", img: SiChakraui },
+	{
+		title: "Node JS",
+		img: imgNode,
+	},
+	{ title: "Pyhton", img: DiPython },
+	{ title: "Django", img: DiDjango },
+	{ title: "Postgresql", img: DiPostgresql },
 ];
--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,6 +1,6 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "samuelleao",
-  whatsappNumber: "+5541111111111",
+	nameUser: "Guilherme Castro",
+	githubUser: "Guilherme-GFC",
+	linkedinUser: "guilherme-gfc",
+	whatsappNumber: "+5545991021542",
 };
